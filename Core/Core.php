82875278b8f05553cbafa25d467395dfcd2fc451
Mici Srsr <micipsa.sersour@epitech.eu>
2021-09-14 15:54:46 +0200

    add dynamic router

--- Core/Core.php
+++ Core/Core.php
@@ -11,7 +11,20 @@ class Core
         $url = $_SERVER['REQUEST_URI'];
         $Router = new Router();
         echo "<pre>";
-        print_r($Router->get($url));
+        if ($Router->get($url) != null){
+            print_r($Router->get($url));
+        }else{
+            $DynamicRouter = new DynamicRouter();
+            $DynamicRouter->get($url);
+            if(isset($controller)){
+                print_r($controller);
+            }
+            echo "<br>";
+            if(isset($action)){
+                print_r($action);
+            }
+        }
+        print_r($DynamicRouter->get($url));
         echo "</pre>";
         echo "<br> url: " . $url;
     }
--- Core/Router.php
+++ Core/Router.php
@@ -8,19 +8,26 @@ class Router
     public static function connect($url, $route)
     {
         self::$routes[$url] = $route;
-        // echo "<pre>";
-        // //print_r(self::$routes);
-        // echo __DIR__;
-        // echo "<br>";
-        // echo "<br>";
-        // echo "</pre>";
-        // // echo "url:" . $url . "<br>";
-        // // print_r($route);
     }
     public static function get($url)
     {
         if (isset(self::$routes[$url])) {
             return self::$routes[$url];
         }
+    }
+}
+
+class DynamicRouter
+{
+    public static function get($url)
+    {
+        $arr = explode("/", $url);
+        array_shift($arr);
+        $arr["controller"] = $arr[0];
+        unset($arr[0]);
+        $arr["action"] = $arr[1];
+        unset($arr[1]);
+        return $arr;
+       
     }
 }
